e893db46a3c01c0ab27efea8f71e68bb7bc857b8
leozhi819 <43287647+leozhi819@users.noreply.github.com>
2019-01-19 20:52:34 +0800

    Add files via upload

---
+++ admin/foot.php
@@ -0,0 +1,3 @@
+<div id="footer"><a href="http://www.0766city.com" target="_blank" style="color:#999999">Copyright © 2018-2020版权所有：阿莫之家解析 备案号：晋ICP备I8888888号-8</span>
+			<br/></a>
+</div> 
---
+++ admin/inc.php
@@ -0,0 +1,5 @@
+<title>阿莫之家影视解析管理后台</title>
+<meta name="keywords" content="阿莫之家影视解析平台" />
+<meta name="description" content="阿莫之家影视解析平台" />
+<link href="./images/admin.css" rel="stylesheet" type="text/css" />
+<link rel="shortcut icon" type="image/x-icon" href="./../favicon.ico"><?php 
